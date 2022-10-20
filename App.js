9deb47cc577340305c73416d99adf0f0186f7ba6
kosarc <petiakraj@gmail.com>
2022-10-21 01:06:47 +0300

    Created Header component and nested inside App

--- App.js
+++ App.js
@@ -1,58 +1,38 @@
 import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
-  const [listName, setListName] = useState([
-    { name: "Peter", id: "1" },
-    { name: "Michel", id: "2" },
-    { name: "Richard", id: "3" },
-    { name: "Frank", id: "4" },
-    { name: "Joe", id: "5" },
-    { name: "Ross", id: "6" },
+  const [toDo, setToDo] = useState([
+    { text: "buy coffee", id: "1" },
+    { text: "create an app", id: "2" },
+    { text: "play on the switch", id: "3" },
   ]);
 
-  const handleOpacityPress = (index) => {
-    console.log(index);
-    setListName((objectOfname) => {
-      return objectOfname.filter((personId) => personId.id != index);
-    });
-  };
   return (
     <View style={styles.container}>
-      <FlatList
-        keyExtractor={(item) => item.id}
-        data={listName}
-        numColumns={2}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleOpacityPress(item.id)}>
-            <Text style={styles.listName}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Header />
+        <View style={styles.content}>
+          {/* Form */}
+          <View style={styles.list}>
+            <FlatList
+              data={toDo}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <Text style={styles.items}>{item.text}</Text>
+              )}
+            />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 80,
+    marginTop: 2,
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  listName: {
-    fontSize: 24,
-    margin: 10,
-    padding: 10,
-    backgroundColor: "lightgrey",
   },
 });
---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>Header</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "coral",
+    height: 60,
+  },
+  title: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 24,
+    height: "100%",
+  },
+});
+
+export default Header;
